16592a38bceadc4edf71af45e4e2e102e5c95d2e
Suraj Vishwakarma <surajvishwakarma625@gmail.com>
2025-11-26 22:44:22 +0530

    landing page update

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,32 @@ import Faq from "./Faq";
 import Feature from "./Feature";
 import Hero from "./Hero";
 import WhyDifferent from "./WhyDifferent";
+import TrustBar from "./TrustBar";
+import ProductPreview from "./ProductPreview";
+import HowItWorks from "./HowItWorks";
+import UseCases from "./UseCases";
+import Testimonials from "./Testimonials";
+import Integrations from "./Integrations";
+import PricingCTA from "./PricingCTA";
+import FinalCTA from "./FinalCTA";
 
 export default function Home() {
   return (
     <PageTransition>
       <main className="flex min-h-screen flex-col items-center justify-between p-24 w-full md:max-w-[1200px] mx-auto py-2 px-4">
         <Hero />
+        <TrustBar />
         <Feature />
+        <ProductPreview />
+        <HowItWorks />
         <WhyDifferent />
+        <UseCases />
         <Chart />
+        <Testimonials />
+        <Integrations />
+        <PricingCTA />
         <Faq />
+        <FinalCTA />
       </main>
     </PageTransition>
   );
